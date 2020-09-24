a6e2489bfc97bf306e045f80249b9ab51baa0e06
Mr. Peng <pengle609@163.com>
2020-09-24 16:28:25 +0800

    2020-9-24 16:28:22

--- app/src/main/java/com/boylab/retrofitdemo/retrofit/RetrofitApi.java
+++ app/src/main/java/com/boylab/retrofitdemo/retrofit/RetrofitApi.java
@@ -8,7 +8,6 @@ import com.boylab.retrofitdemo.retrofit.model.LoginUserResponse;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
